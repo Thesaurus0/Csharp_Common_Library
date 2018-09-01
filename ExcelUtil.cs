73a5848cdc7c79bb779cdbf6837d22662cebbcc3
Richard <wdk1231@163.com>
2018-09-01 22:33:21 +0800

    a

--- ExcelUtil.cs
+++ ExcelUtil.cs
@@ -6,20 +6,97 @@ using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading.Tasks;
 using Excel = Microsoft.Office.Interop.Excel;
+using System.IO;
+using System.Windows.Forms;
 
 namespace CommonLib
 {
     public class ExcelUtil
     {
         private const string SHEET_CODE_NAME = "SHEET_CODE_NAME";
-        private static Excel.Application _ExcelApp = GetExcelApplication();
 
+        #region windows API
+        [DllImport("ole32.dll")]
+        public static extern void GetRunningObjectTable(int reserved, out IRunningObjectTable prot);
+        [DllImport("ole32.dll")]
+        static extern int CreateBindCtx(uint reserved, out IBindCtx ppbc);
+        #endregion
+
+        #region _ExcelApp
+        private static Excel.Application _ExcelApp = null;
         public static Excel.Application ExcelAPP
         {
             get { return _ExcelApp; }
+            set { _ExcelApp = value; }
+        }
+        #endregion
+
+        #region workbook operation
+
+        public static Excel.Workbook CreateWorkbook(string excelFileFullPath, string sheetName = null)
+        {
+            Excel.Workbook wb = null;
+
+            if (File.Exists(excelFileFullPath))
+            {
+                DialogResult response = MessageBox.Show(text:"The file already exists, do you want to replace it with the new one?" + Environment.NewLine + excelFileFullPath 
+                    , caption:"File already exists", buttons: MessageBoxButtons.YesNoCancel, icon: MessageBoxIcon.Question);
+                if (response != DialogResult.Yes)
+                    throw new Exception("User abort");
+            }
+
+            _ExcelApp.SheetsInNewWorkbook = 1;
+            wb = _ExcelApp.Workbooks.Add(Excel.XlWBATemplate.xlWBATWorksheet);
+            _ExcelApp.ActiveWindow.DisplayGridlines = false;
+
+            wb.Worksheets[1].Name = sheetName ?? Path.GetFileNameWithoutExtension(excelFileFullPath);
+
+            Excel.XlFileFormat fileFormat = GetExcelFileFormatByFileExtension(excelFileFullPath);
+            wb.SaveAs(Filename: excelFileFullPath, FileFormat: fileFormat);
+            return wb;
         }
 
-        #region GetSheetBySheetName
+        public static Excel.Workbook GetActiveWorkbook()
+        {
+            return GetExcelApplication()?.ActiveWorkbook;
+        }
+
+        private static Excel.XlFileFormat GetExcelFileFormatByFileExtension(string excelFileFullPath)
+        {
+            Excel.XlFileFormat fileFormat = default(Excel.XlFileFormat);
+            string ext = Path.GetExtension(excelFileFullPath).ToUpper();
+
+            switch (ext)
+            {
+                case ".CSV":
+                    fileFormat = Excel.XlFileFormat.xlCSV;
+                    break;
+                case ".XLS":
+                    fileFormat = Excel.XlFileFormat.xlExcel8;
+                    break;
+                case ".XLSX":
+                    fileFormat = Excel.XlFileFormat.xlOpenXMLWorkbook ;
+                    break;
+                case ".XLSM":
+                    fileFormat = Excel.XlFileFormat.xlOpenXMLWorkbookMacroEnabled;
+                    break;
+                case ".XLSB":
+                    fileFormat = Excel.XlFileFormat.xlExcel12;
+                    break;
+                case ".TXT":
+                    fileFormat = Excel.XlFileFormat.xlCurrentPlatformText;
+                    break;
+                case ".PRN":
+                    fileFormat = Excel.XlFileFormat.xlTextPrinter;
+                    break;
+                default:
+                    throw new Exception("File extension is invalid :" + ext.ToLower());
+                    //break;
+            }
+            return fileFormat;
+        }
+        #endregion
+
         public static Excel.Worksheet GetSheetBySheetName(string shtName, Excel.Workbook wb = null)
         {
             Excel.Worksheet shtOut = null;
@@ -36,9 +113,7 @@ namespace CommonLib
             }
             return shtOut;
         }
-        #endregion
 
-        #region GetSheetByCodeName
         public static Excel.Worksheet GetSheetByCodeName(string shtCodeName, Excel.Workbook wb = null)
         {
             wb = SetParamWorkbookToAcitveWorkbookWhenItIsNull(wb);
@@ -52,8 +127,6 @@ namespace CommonLib
             }
             return null;
         }
-        #endregion
-        #region GetRangeByStartEndPos
         public static Excel.Range GetRangeByStartEndPos(Excel.Worksheet sht, long StartRow, long StartCol, long EndRow, long EndCol)
         {
             Excel.Range rg = null;
@@ -66,9 +139,7 @@ namespace CommonLib
             rg = sht.Range[sht.Cells[StartRow, StartCol], sht.Cells[EndRow, EndCol]];
             return rg;
         }
-        #endregion
 
-        #region SetParamWbToAcitveWorkbookWhenItIsNull
         private static Excel.Workbook SetParamWorkbookToAcitveWorkbookWhenItIsNull(Excel.Workbook wb)
         {
             if (wb != null)
@@ -87,59 +158,18 @@ namespace CommonLib
                 }
             }
         }
-        #endregion
 
-        #region GetExcelApplication
-        private static Excel.Application GetExcelApplication(bool MultiInstanceThenGetTheDefault = false)
-        {
-            Excel.Application xlApp = null;
 
-            if (!MultiInstanceThenGetTheDefault)
-            {
-                try
-                {
-                    MultipleExcelApplicationIsRunning();
-                }
-                catch (Exception excp)
-                {
-                    System.Windows.Forms.MessageBox.Show(excp.Message);
-                    throw excp;
-                }
-            }
-
-            try
-            {
-                xlApp = (Excel.Application)Marshal.GetActiveObject("Excel.Application");
-            }
-            catch (Exception excp)
-            {
-                xlApp = new Excel.Application();
-                //throw excp;
-            }
-            finally
-            {
-                xlApp.Visible = true;
-            }
-
-            return xlApp;
-        }
-        #endregion
-
-        #region GetSheetCodeName
         public static string GetSheetCodeName(Excel.Worksheet sht)
         {
             return GetSheetProperty(sht, SHEET_CODE_NAME);
         }
-        #endregion
 
-        #region SetSheetCodeName
         public static void SetSheetCodeName(Excel.Worksheet sht, string shtCodeName)
         {
             SetSheetProperty(sht, SHEET_CODE_NAME, shtCodeName);
         }
-        #endregion
 
-        #region SetSheetProperty
         private static void SetSheetProperty(Excel.Worksheet sht, string propertyName, string propertyValue)
         {
             bool existing = false;
@@ -157,7 +187,6 @@ namespace CommonLib
             if (!existing)
                 sht.CustomProperties.Add(propertyName, propertyValue);
         }
-        #endregion
 
         #region GetSheetProperty
         private static string GetSheetProperty(Excel.Worksheet sht, string propertyName)
@@ -173,146 +202,224 @@ namespace CommonLib
         }
         #endregion
 
-        //private static Excel.Application GetExcelApplications()
+        #region comment out
+        //public static bool MultipleExcelApplicationIsRunning(bool ThrowsExceptionAtOnce = true)
         //{
+        //    int instanceCnt = 0;
+
+        //    List<object> instances = GetRunningInstances(new string[1] { "Excel.Application" });
+        //    instanceCnt = instances.Count;
+        //    if (instanceCnt <= 1)
+        //        return false;
+
         //    Excel.Application xlApp = null;
+        //    foreach (var item in instances)
+        //    {
+        //        bool toRelease = false;
 
-        //    [DllImport("ole32.dll")]
-        //static extern int CreateBindCtx(uint reserved, out IBindCtx ppbc);
+        //        if (item is Excel.Application)
+        //        { 
+        //            xlApp = (item as Excel.Application);
+        //            xlApp.Visible = true;
+        //        }
 
-        //[DllImport("ole32.dll")]
+        //        if (xlApp.Visible == true)
+        //        {
+        //            if (xlApp.ActiveWorkbook.Name == null)
+        //                toRelease = true;
+        //        }
+        //        else
+        //        {
+        //            toRelease = true;
+        //        }
 
-        //public static extern void GetRunningObjectTable(int reserved, out IRunningObjectTable prot);
+        //        if (toRelease)
+        //        {
+        //            instanceCnt--;
+        //            if (instanceCnt > 0)
+        //            {
+        //                xlApp.Quit();
+        //                Marshal.ReleaseComObject(xlApp);
+        //            }
+        //            else
+        //            {
+        //                return false;
+        //            }
+        //        }
+        //    }
 
-        //    return xlApp;
+
+        //    if (instanceCnt > 1)
+        //    {
+        //        if (ThrowsExceptionAtOnce)
+        //            throw new Exception("Multiple Excel instance is running, you may now try to get the activeworkook, so that program cannot determine which instance's activeworkook you want. ");
+        //    }
+
+        //    return instanceCnt > 1;
         //}
 
-        #region MultipleExcelApplicationIsRunning
-        public static bool MultipleExcelApplicationIsRunning(bool ThrowsExceptionAtOnce = true)
+
+
+        // Requires Using System.Runtime.InteropServices.ComTypes
+        // Get all running instance by querying ROT
+        //private static List<object> GetRunningInstances(string[] progIds)
+        //{
+        //    List<string> clsIds = new List<string>();
+        //    // get the app clsid
+        //    foreach (string progId in progIds)
+        //    {
+        //        Type type = Type.GetTypeFromProgID(progId);
+
+        //        if (type != null)
+        //            clsIds.Add(type.GUID.ToString().ToUpper());
+        //    }
+        //    // get Running Object Table ...
+        //    IRunningObjectTable Rot = null;
+        //    GetRunningObjectTable(0, out Rot);
+        //    if (Rot == null)
+        //        return null;
+        //    // get enumerator for ROT entries
+        //    IEnumMoniker monikerEnumerator = null;
+
+        //    Rot.EnumRunning(out monikerEnumerator);
+        //    if (monikerEnumerator == null)
+        //        return null;
+        //    monikerEnumerator.Reset();
+
+        //    List<object> instances = new List<object>();
+
+        //    IntPtr pNumFetched = new IntPtr();
+
+        //    IMoniker[] monikers = new IMoniker[1];
+
+        //    // go through all entries and identifies app instances
+        //    while (monikerEnumerator.Next(1, monikers, pNumFetched) == 0)
+        //    {
+        //        IBindCtx bindCtx;
+        //        CreateBindCtx(0, out bindCtx);
+        //        if (bindCtx == null)
+        //            continue;
+
+        //        string displayName;
+        //        monikers[0].GetDisplayName(bindCtx, null, out displayName);
+
+        //        foreach (string clsId in clsIds)
+        //        {
+        //            if (displayName.ToUpper().IndexOf(clsId.ToUpper()) > 0)
+        //            {
+        //                object ComObject;
+
+        //                Rot.GetObject(monikers[0], out ComObject);
+
+        //                if (ComObject == null)
+        //                    continue;
+        //                instances.Add(ComObject);
+
+        //                //var v = typeof(   );
+        //                break;
+        //            }
+        //        }
+        //    }
+        //    return instances;
+        //}
+        #endregion
+
+        #region Excel Application Operation
+        private static List<Excel.Application> GetAllExcelApplicationInstances()
         {
-            int instanceCnt = 0;
+            HashSet<Excel.Application> result = new HashSet<Excel.Application>();
 
-            List<object> instances = GetRunningInstances(new string[1] { "Excel.Application" });
-            instanceCnt = instances.Count;
-            if (instanceCnt <= 1)
-                return false;
+            IRunningObjectTable Rot;
+            GetRunningObjectTable(0, out Rot);
 
-            Excel.Application xlApp = null;
-            foreach (var item in instances)
+            IEnumMoniker monikerEnumerator = null;
+            Rot.EnumRunning(out monikerEnumerator);
+
+            IntPtr iFetched = new IntPtr();
+            IMoniker[] monikers = new IMoniker[1];
+
+
+            IBindCtx ctx;
+            CreateBindCtx(0, out ctx);
+
+            while (monikerEnumerator.Next(1, monikers, iFetched) == 0)
             {
-                bool toRelease = false;
-
-                if (item is Excel.Application)
-                { 
-                    xlApp = (item as Excel.Application);
-                    xlApp.Visible = true;
-                }
-
-                if (xlApp.Visible == true)
+                string appName = "";
+                dynamic wb = null;
+                try
                 {
-                    if (xlApp.ActiveWorkbook.Name == null)
-                        toRelease = true;
+                    Guid iUnknown = new Guid("{00000000-0000-0000-C000-000000000046}");
+                    monikers[0].BindToObject(ctx, null, ref iUnknown, out wb);
+                    //appName = wb.Applicaiton.Name;
+                    appName = wb.Application.Name;
                 }
-                else
+                catch { }
+                if (appName == "Microsoft Excel")
                 {
-                    toRelease = true;
-                }
+                    Excel.Application xlApp = wb.Application;
 
-                if (toRelease)
-                {
-                    instanceCnt--;
-                    if (instanceCnt > 0)
+                    if (!xlApp.Visible)
                     {
                         xlApp.Quit();
                         Marshal.ReleaseComObject(xlApp);
                     }
                     else
                     {
-                        return false;
+                        if (!result.Contains(xlApp))
+                        {
+                            result.Add(xlApp);
+                        }
                     }
                 }
             }
-
-
-            if (instanceCnt > 1)
-            {
-                if (ThrowsExceptionAtOnce)
-                    throw new Exception("Multiple Excel instance is running, you may now try to get the activeworkook, so that program cannot determine which instance's activeworkook you want. ");
-            }
-
-            return instanceCnt > 1;
+            return result.ToList();
         }
-        #endregion
 
-        [DllImport("ole32.dll")]
-        static extern int CreateBindCtx(uint reserved, out IBindCtx ppbc);
-
-        [DllImport("ole32.dll")]
-        public static extern void GetRunningObjectTable(int reserved, out IRunningObjectTable prot);
-
-        #region GetRunningInstances
-
-        // Requires Using System.Runtime.InteropServices.ComTypes
-        // Get all running instance by querying ROT
-        private static List<object> GetRunningInstances(string[] progIds)
+        private static Excel.Application GetDefaultFirstApplication()
         {
-            List<string> clsIds = new List<string>();
-            // get the app clsid
-            foreach (string progId in progIds)
-            {
-                Type type = Type.GetTypeFromProgID(progId);
+            Excel.Application xlApp = null;
 
-                if (type != null)
-                    clsIds.Add(type.GUID.ToString().ToUpper());
+            try
+            {
+                xlApp = (Excel.Application)Marshal.GetActiveObject("Excel.Application");
             }
-            // get Running Object Table ...
-            IRunningObjectTable Rot = null;
-            GetRunningObjectTable(0, out Rot);
-            if (Rot == null)
-                return null;
-            // get enumerator for ROT entries
-            IEnumMoniker monikerEnumerator = null;
-
-            Rot.EnumRunning(out monikerEnumerator);
-            if (monikerEnumerator == null)
-                return null;
-            monikerEnumerator.Reset();
-
-            List<object> instances = new List<object>();
-
-            IntPtr pNumFetched = new IntPtr();
-
-            IMoniker[] monikers = new IMoniker[1];
-
-            // go through all entries and identifies app instances
-            while (monikerEnumerator.Next(1, monikers, pNumFetched) == 0)
+            catch (Exception)
             {
-                IBindCtx bindCtx;
-                CreateBindCtx(0, out bindCtx);
-                if (bindCtx == null)
-                    continue;
+                xlApp = new Excel.Application();
+            }
+            finally
+            {
+                xlApp.Visible = true;
+            }
+            return xlApp;
+        }
+        private static Excel.Application GetExcelApplication(bool MultiInstanceThenGetTheDefault = false)
+        {
+            if (_ExcelApp != null)
+                return _ExcelApp;
 
-                string displayName;
-                monikers[0].GetDisplayName(bindCtx, null, out displayName);
+            int excelAppInstanceNum = GetAllExcelApplicationInstances().Count;
 
-                foreach (string clsId in clsIds)
+            Excel.Application xlApp = null;
+
+            if (excelAppInstanceNum <= 1)
+            {
+                xlApp = GetDefaultFirstApplication();
+                xlApp.Visible = true;
+            }
+            else
+            {
+                if (!MultiInstanceThenGetTheDefault)
                 {
-                    if (displayName.ToUpper().IndexOf(clsId.ToUpper()) > 0)
-                    {
-                        object ComObject;
-
-                        Rot.GetObject(monikers[0], out ComObject);
-
-                        if (ComObject == null)
-                            continue;
-                        instances.Add(ComObject);
-
-                        //var v = typeof(   );
-                        break;
-                    }
+                    xlApp = GetDefaultFirstApplication();
+                    xlApp.Visible = true;
+                }
+                else
+                {
+                    throw new Exception("Multiple Excel instances are running, please change your program to get the excel application directly " + Environment.NewLine + "Like moniker.bindtomonier(filename), or set this _xlapp");
                 }
             }
-            return instances;
+            return xlApp;
         }
         #endregion
     }
